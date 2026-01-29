536b5100a9ee128efc1b6675ae22f2d41accc0cd
AsokTamang <ashoktmg2058@gmail.com>
2026-01-28 20:24:29 -0500

    linear transformation using numpy

---
+++ matrix_multiplication/linear_transformation.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def T(v):
+    w = np.zeros((3, 1))
+    w[0, 0] = 3 * v[0, 0]
+    w[2, 0] = -2 * v[1, 0]
+
+    return w
+
+
+v = np.array([[3], [5]])
+w = T(v)
+
+print("Original vector:\n", v, "\n\n Result of the transformation using the function T is:\n", w)
--- matrix_multiplication/matrix_mul.py
+++ matrix_multiplication/matrix_mul.py
@@ -7,3 +7,9 @@ B = np.array([[2, 2], [5, 7], [4, 4]])
 print("Matrix B (3 by 2):\n", B)
 
 print(np.matmul(A, B))  #using the api matmul
+#inorder for the matrix multiplication between two matrices A and B to be possible, the columns of the matrix A or first matrix must be equal to the row of matrix B or second matrix.
+print(A.ndim)  #here as our array A has rows and columns, so it is a two-dimensional array
+
+print(np.zeros((3,1)))
+
+
