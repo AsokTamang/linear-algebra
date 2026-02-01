8635afd3320420a775c4dcd66663110f200a4931
AsokTamang <ashoktmg2058@gmail.com>
2026-01-31 22:56:11 -0500

    visual representation for intuition

--- pca/application_pca.py
+++ pca/application_pca.py
@@ -21,7 +21,7 @@ def center_data(Y):
     mean_vector = np.mean(Y, axis=0)  # axis=0 means finding the mean column wise, as the Y consists the number of observations in a row and the number of pixels in a column
     mean_matrix = Y - mean_vector  #centering the data, which means subtracting the mean from the variable column wise
     # use np.reshape to reshape into a matrix with the same size as Y. Remember to use order='F'
-    mean_matrix = np.reshape(mean_matrix,Y.shape,order='F')
+    mean_matrix = np.reshape(mean_matrix,Y.shape,order='F')  #reshaping the matrix as same as the original dataset
 
     X = mean_matrix
     return X
@@ -50,8 +50,30 @@ print('Eigen vectors are:', eigenvecs)
 
 def perform_PCA(X, eigenvecs, k):
     V = eigenvecs[:, :k]  #choosing the eigen vectors , k PCAs means largest and second largest eigen values
+    #reducing the original dataset into K components or k dimension
     Xred = X @ V  #centered dataset dot product eigen vector(K PCAs)
     ### END CODE HERE ###
     return Xred
 
+
+
+def restructure_image(xred,eigenvecs):
+    #converting the reduced dataset into approx equal original dataset
+    x_reconstructed = xred @ (eigenvecs[:,:xred.shape[1]]).T  #here we must use the same number of PCs(Principal Component) used to reduce the original dataset
+    return x_reconstructed
+
+
+# all the remaining eigenvalues of the covariance matrix are zero, and thus won't add enything to the explained variance, after the 55 Principal Components
+
+explained_variance = eigenvals/sum(eigenvals)   #here explained_variance gives us the how much variance is explained by each princial component
+plt.plot(np.arange(1,56), explained_variance)   #x-axis represents the number of PCAs and Y-axis represents the amount of variance in the current x-axis PCA
+plt.show()
+
+
+#np.cumsum calculates the variance explained upto the specific principal component
+explained_cum_variance = np.cumsum(explained_variance)
+#but in case of cumulative explained variance , the x-axix represents the number of PCAs and Y-axis represents the percentage of variance upto X-axis PCA
+plt.plot(np.arange(1,56), explained_cum_variance)
+plt.axhline(y=0.99, color='r')  #now here we are checking how many PCAs to keep, for getting 99% variance, which can be obtained using the intersection point
+plt.show()
 
