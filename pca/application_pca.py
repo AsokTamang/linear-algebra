8934f887ee84c736a099fe142802e5512243d531
AsokTamang <ashoktmg2058@gmail.com>
2026-01-31 20:40:05 -0500

    function to center the data in a matrix

--- pca/application_pca.py
+++ pca/application_pca.py
@@ -1,4 +1,27 @@
+import numpy as np
+
 import utils
+import matplotlib.pyplot as plt
 
 imgs = utils.load_images('../data/')
-print(imgs)
+print(imgs[0].reshape(-1))  #flattening the image in one array
+height, width = imgs[0].shape
+
+print(f'\nour dataset has {len(imgs)} images of size {height}x{width} pixels\n')
+plt.imshow(imgs[0], cmap='gray')
+plt.show()
+
+flatten_images = np.array([img.reshape(-1) for img in imgs])  #flattening the image's pixels into single array
+
+
+#centering the data of matrix
+# Graded cell
+def center_data(Y):
+
+    mean_vector = np.mean(Y, axis=0)  # axis=0 means finding the mean column wise, as the Y consists the number of observations in a row and the number of pixels in a column
+    mean_matrix = Y - mean_vector  #centering the data, which means subtracting the mean from the variable column wise
+    # use np.reshape to reshape into a matrix with the same size as Y. Remember to use order='F'
+    mean_matrix = np.reshape(mean_matrix,Y.shape,order='F')
+
+    X = mean_matrix
+    return X
